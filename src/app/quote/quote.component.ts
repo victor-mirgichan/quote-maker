cfc0fed73990c07b8d21002a480f3880150e111d
victor-mirgichan <provic13@gmail.comcd>
2020-08-26 12:17:59 +0300

    Added form content

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -6,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  quotes = [
+    new Quote(1, 'Nelson Mandela', 'There is no passion to be found playing small - in settling for a life that is less than the one you are capable of living.', 'admin', new Date(1990, 5, 10)),
+    new Quote(2, 'Mohandas Gandhi', 'Strength does not come from physical capacity. It comes from an indomitable will.', 'admin', new Date(1950, 3, 23)),
+    new Quote(3, 'Rick Flores', 'We have to come for them before they come for us, it is that simple', 'admin', new Date(2012, 8, 17)),
 
-  ngOnInit(): void {
+  ]
+
+showDescription= false;//boolean setting to hide and display
+
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
+
+    deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Do you want to delete this Quote? ${this.quotes[index].name}`)//alert
+
+      if (toDelete) { this.quotes.splice(index, 1) }//removes quote
+    }
+  }
+
+  toggleQuote() {
+    this.showDescription = !this.showDescription;//show or hide content
+  }
+
+
+
+ngOnInit() {
+}
 
 }
